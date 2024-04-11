683a0d632c844ea701812be4814331e512b0fdd7
Mariusz Schimke <mariusz.schimke@gmail.com>
2024-04-11 12:23:00 +0200

    Add comments

--- src/SensitiveString/Json/JsonSerializerOptionsExtensions.cs
+++ src/SensitiveString/Json/JsonSerializerOptionsExtensions.cs
@@ -4,6 +4,12 @@ namespace SensitiveString.Json;
 
 public static class JsonSerializerOptionsExtensions
 {
+    /// <summary>
+    ///     Adds <see cref="SensitiveString" /> and <see cref="SensitiveEmail" /> to the serializer options.
+    /// </summary>
+    /// <param name="options">
+    ///     The serializer options to add sensitive string support to.
+    /// </param>
     public static void AddSensitiveStringSupport(this JsonSerializerOptions options)
     {
         options.Converters.Add(new SensitiveStringJsonConverter());
