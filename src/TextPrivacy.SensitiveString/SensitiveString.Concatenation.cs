56e42f9daf3742005fb81debca20f4ca4becdd50
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-02-27 18:17:27 +0100

    Cleanup

--- src/TextPrivacy.SensitiveString/SensitiveString.Concatenation.cs
+++ src/TextPrivacy.SensitiveString/SensitiveString.Concatenation.cs
@@ -3,11 +3,11 @@ namespace TextPrivacy.SensitiveString;
 // remark: two concatenated null strings result in an empty string!
 public partial class SensitiveString
 {
-    public static SensitiveString operator +(SensitiveString? left, SensitiveString? right) => new(left?.Reveal() + right?.Reveal());
+    public static SensitiveString operator +(SensitiveString? left, SensitiveString? right) => new(left?._getValue() + right?._getValue());
 
-    public static SensitiveString operator +(SensitiveString? left, string? right) => new(left?.Reveal() + right);
-    public static SensitiveString operator +(string? left, SensitiveString? right) => new(left + right?.Reveal());
+    public static SensitiveString operator +(SensitiveString? left, string? right) => new(left?._getValue() + right);
+    public static SensitiveString operator +(string? left, SensitiveString? right) => new(left + right?._getValue());
 
-    public static SensitiveString operator +(SensitiveString? left, object? right) => new(left?.Reveal() + right);
-    public static SensitiveString operator +(object? left, SensitiveString? right) => new(left + right?.Reveal());
+    public static SensitiveString operator +(SensitiveString? left, object? right) => new(left?._getValue() + right);
+    public static SensitiveString operator +(object? left, SensitiveString? right) => new(left + right?._getValue());
 }
