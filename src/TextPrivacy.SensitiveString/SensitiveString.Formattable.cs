5053509ca7060f6d5963ecd4b3353f4d3a5298b5
Mariusz Schimke <mariusz.schimke@gmail.com>
2025-03-01 13:55:18 +0100

    Update comment

--- src/TextPrivacy.SensitiveString/SensitiveString.Formattable.cs
+++ src/TextPrivacy.SensitiveString/SensitiveString.Formattable.cs
@@ -4,16 +4,19 @@ public partial class SensitiveString : IFormattable
 {
     /// <summary>
     ///     Returns a string representation of the value with optional formatting.
-    ///     The following formats are supported:
+    ///     The following formats are supported (case-insensitive):
     ///     <list type="bullet">
     ///         <item>
-    ///             <description><c>R</c> – Reveals the original value.</description>
+    ///             <description><c>R</c> or <c>null</c> – Reveals the original value.</description>
     ///         </item>
     ///         <item>
-    ///             <description><c>M:</c> – Applies a custom mask instead of the default one, e.g. <c>M:*****</c>.</description>
+    ///             <description><c>M</c> – Masks the value with the default mask.</description>
+    ///         </item>
+    ///         <item>
+    ///             <description><c>M:</c> – Applies a custom mask instead of the default one, e.g. <c>M:*</c> will return <c>*</c>.</description>
     ///         </item>
     ///     </list>
-    ///     If the format is not recognized, the default masking is applied, and the value remains hidden.
+    ///     If the format is not recognized, an exception is thrown.
     /// </summary>
     /// <param name="format">
     ///     The format specifier.
@@ -21,6 +24,9 @@ public partial class SensitiveString : IFormattable
     /// <param name="formatProvider">
     ///     A format provider (currently unused).
     /// </param>
+    /// <exception cref="FormatException">
+    ///     Thrown when the format is not recognized.
+    /// </exception>
     public string ToString(string? format, IFormatProvider? formatProvider)
     {
         return format?.ToLowerInvariant() switch
@@ -28,7 +34,7 @@ public partial class SensitiveString : IFormattable
             null or "r" => _getValue(), // revealed
             ['m', ':', .. var mask] => mask, // return the specified mask
             "m" => ToString(), // default masking 
-            _ => throw new FormatException($"The format '{format}' is not supported")
+            _ => throw new FormatException($"The format '{format}' is not supported.")
         };
     }
 
